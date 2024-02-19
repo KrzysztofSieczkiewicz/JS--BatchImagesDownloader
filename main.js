3957b627c7e30311c32a6e18e4ebdf98f9883959
Kristoff <krzysztof.sieczkiewicz@gmail.com>
2024-02-19 22:20:56 +0100

    creating index.html

--- main.js
+++ main.js
@@ -1,18 +1,68 @@
-const { app, BrowserWindow } = require('electron');
+const path = require('path')
+const { app, BrowserWindow, Menu } = require('electron');
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+const isDev = process.env.NODE_ENV !== 'development'
+const isMac = process.platform === 'darwin';
+const isWindows = process.platform === 'win32';
+const isLinux = process.platform === 'linux';
 
-  win.loadFile('src/index.html')
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+      title: 'Batch image downloader',
+      width: isDev ? 1000 : 500,
+      height: 600,
+    });
+
+  const appMenu = Menu.buildFromTemplate(menu);
+  mainWindow.setMenu(appMenu);
+
+    // Open devtools if in dev env
+    if(isDev) {
+      mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
-app.whenReady().then(() => {
-  createWindow()
+function createHelpWindow() {
+  const helpWindow = new BrowserWindow({
+    title: 'How to use?',
+    width: 600,
+    height: 300,
+  });
+
+  helpWindow.setMenu(null);
+
+  helpWindow.loadFile(path.join(__dirname, './renderer/help.html'));
+}
+
+app.whenReady().then( () => {
+  createMainWindow();
+
+  app.on('activate', () => {
+    if(BrowserWindow.getAllWindows.length === 0) {
+      createMainWindow();
+    }
+  })
+  
+  app.on('window-all-closed', () => {
+    if(!isMac) {
+      app.quit();
+    }
+  })
+
 })
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+// Top bar menu template
+const menu = [
+  {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'How to?',
+        click: () => createHelpWindow(),
+        accelerator: 'CmdOrCtrl+H'
+      }
+    ]
+  }
+]
